c4cd0edc2c3dd9daffd15cacc28065704a9f90db
etLanaBanana <svetlanaskovoroda@gmail.ru>
2023-09-16 23:28:10 +0300

    Caesar

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,43 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите действие: \n1.зашифровать текст.\n2.разшифровать текс.");
+        int number = scanner.nextInt();
+        if (number==1) {
+            zashifrovka();
+        } else if (number==2) {
+            razshifrovka();
+        } else {
+            System.out.println("Вы что-то неправильно ввели, выберите цифру 1 или 2");
         }
+
+
+    }
+    public static void zashifrovka() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст, который хотите зашифровать");
+        String text = scanner.nextLine();
+        System.out.println("Введите ключ");
+        int key = scanner.nextInt();
+        char[] shifr = new char[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+                shifr[i] = (char) ((int)(text.charAt(i)) + key);
+            }
+            System.out.println(shifr);
+    }
+    public static void razshifrovka() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст, который хотите разшифровать");
+        String text = scanner.nextLine();
+        System.out.println("Введите ключ");
+        int key = scanner.nextInt();
+        char[] shifr = new char[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            shifr[i] = (char) ((int)(text.charAt(i)) - key);
+        }
+        System.out.println(shifr);
     }
 }
